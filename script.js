60b9af0d8d6ea13534401b275f0446c47fb7fec6
Subhadeep Hazra <124555668+subhadeephazra@users.noreply.github.com>
2023-08-29 11:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 window.promises = [];
-for (let i = 0; i < 5; i++) {
-    const randomTime = getRandomTime(1000, 5000); // Time in milliseconds (1 to 5 seconds)
-    const promise = new Promise(resolve => {
-        setTimeout(() => {
-            resolve(`Promise ${i + 1} resolved after ${randomTime}ms`);
-        }, randomTime);
-    });
-    promises.push(promise);
-}
-
-Promise.any(promises)
-    .then(result => {
-        const outputDiv = document.getElementById('output');
-        outputDiv.textContent = result;
-    })
-    .catch(error => {
-        console.error('All promises failed:', error);
-    });
 
 // Do not change the code above this
 // add your promises to the array `promises`
+function getRandomTime(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+function createRandomPromise() {
+	const randomTime = getRandomTime(1000, 5000);
+    return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(`Promise resolved after ${randomTime}ms`);
+		}, randomTime);
+	});
+}
+
+    // Add your promises to the existing promises array
+for (let i = 0; i < 5; i++) {
+	promises.push(createRandomPromise());
+}
+Promise.any(promises)
+	.then((result) => {
+		const div = document.getElementById("output");
+		div.textContent = result;
+	})
+	.catch((error) => {
+		console.error("All promises rejected:", error);
+	});
